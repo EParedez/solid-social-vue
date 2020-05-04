c39aef44ba91f22342d0a9f79c43b17faa87a6e4
Emanuell Paredes <emanuellp@hotmail.com>
2020-05-03 23:01:00 -0500

    init

--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import Home from "./pages/Home";
+import Login from "./components/Login";
+
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 Vue.config.productionTip = false
 
+const routes = [
+  { path: '/', component: Home },
+  { path: '/login', component: Login }
+]
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  routes
+})
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
